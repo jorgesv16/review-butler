523141fcdfb8bd226bc1a2ccee4197a0c0ed6194
William Brooks <brooks.william@gmail.com>
2017-12-20 19:37:29 -0800

    added static cover page and static app page

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,12 @@ import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import { Container } from "./components/Grid";
 
-const App = () =>
+const App = () => (
   <Router>
     <div className="container">
-      <Nav />
-      <Container fluid>
-            <Switch>
-              <Route exact path="/" component={Main} />
-              <Route exact path="/main" component={Main} />
-              <Route exact path="/main/:id" component={Main} />
-              <Route component={NoMatch} />
-            </Switch>
-      </Container>
-      <footer className="footer text-center">
-        <p className="text-muted small mb-0">Copyright &copy; João Paulo Alexandre de Oliveira 2017</p>
-      </footer>
+      <Container fluid>Insert Review here</Container>
     </div>
-  </Router>;
+  </Router>
+);
 
 export default App;
