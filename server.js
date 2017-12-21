78e4f3eca27b5a7d6ac87c5a52de28bf6e30b999
David <djlowes@gmail.com>
2017-12-21 11:17:10 -0800

    passport setup and removed old files

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const mongoose = require("mongoose");
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session')({ secret: 'I have no secrets', resave: false,
+ saveUninitialized: false });
+const User = require('./models/user');
+
+// Routes
 const routes = require("./routes");
+
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressSession);
+app.use(passport.initialize());
+app.use(passport.session());
 // Serve up static assets
 app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
+
+// Configure passport
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Set up promises with mongoose
 mongoose.Promise = global.Promise;
