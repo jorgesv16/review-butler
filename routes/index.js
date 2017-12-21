87fecd50f758d419bcc4fc36a05303cb83c5460a
William Brooks <brooks.william@gmail.com>
2017-12-21 11:48:25 -0800

    put back articles controller so we can use as an example and put back index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const path = require("path");
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-const router = express.Router();
+// API Routes
+router.use("/api", apiRoutes);
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Review Butler' });
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
