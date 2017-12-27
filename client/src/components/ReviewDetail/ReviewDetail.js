f47f9d8543774013c3f722ec76e1c44cb08ca855
William Brooks <brooks.william@gmail.com>
2017-12-26 19:02:09 -0800

    added phrases to seed file

--- client/src/components/ReviewDetail/ReviewDetail.js
+++ client/src/components/ReviewDetail/ReviewDetail.js
@@ -18,18 +18,19 @@ const ReviewDetail = ({ review }) => {
 		<div className="float-md-left">
 			<div className="review-container full-height review-detail">
 				<p><strong>title: </strong>{review.title}</p>
-				<p><strong>user_name: </strong>{review.user_name}</p>
-				<p><strong>Rating: </strong>{review.Rating}{stars}</p>
-				<p><strong>sentiment_score: </strong>{review.sentiment_score}</p>
-				<p><strong>word_count: </strong>{review.word_count}</p>
-				<p><strong>review_date: </strong><Moment date={review.review_date} format="MM/DD/YYYY"/></p>
-				<p><strong>response_date: </strong>{review.response_date}</p>
-				<p><strong>photo: </strong>{review.photo}</p>
-				<p><strong>tags: </strong>{review.tags}</p>
-				<p><strong>review_read: </strong>{review.review_read}</p>
-				<p><strong>business_id: </strong>{review.business_id}</p>
-				<p><strong>timestamp: </strong>{review.timestamp}</p>
-				<p><strong>external_id: </strong>{review.external_id}</p>
+				<p><strong>text: </strong>{review.text}</p>
+				<li><strong>user_name: </strong>{review.user_name}</li>
+				<li><strong>Rating: </strong>{review.Rating}{stars}</li>
+				<li><strong>sentiment_score: </strong>{review.sentiment_score}</li>
+				<li><strong>word_count: </strong>{review.word_count}</li>
+				<li><strong>review_date: </strong><Moment date={review.review_date} format="MM/DD/YYYY"/></li>
+				<li><strong>response_date: </strong>{review.response_date}</li>
+				<li><strong>photo: </strong>{review.photo}</li>
+				<li><strong>tags: </strong>{review.tags}</li>
+				<li><strong>review_read: </strong>{review.review_read}</li>
+				<li><strong>business_id: </strong>{review.business_id}</li>
+				<li><strong>timestamp: </strong>{review.timestamp}</li>
+				<li><strong>external_id: </strong>{review.external_id}</li>
 				
 			</div>
 		</div>
