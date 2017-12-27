3a7db5e4119cb81a358f814cf35ce46db3c9bc5c
David <djlowes@gmail.com>
2017-12-27 14:07:17 -0800

    serve static HTML

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 import Sidebar from "./components/Sidebar";
 import Inbox from "./pages/Inbox";
+import StaticContainer from "./components/StaticContainer";
 
 const App = () => (
 	<Router>
@@ -13,6 +14,7 @@ const App = () => (
 			<Route exact path="/dashboard" component={Dashboard} />
 			<Route exact path="/settings" component={Settings} />
 			<Route exact path="/inbox" component={Inbox} />
+			<Route name="signup" exact path="/signup" component={StaticContainer} />
 		</Switch>
 	</div>
 	</Router>
