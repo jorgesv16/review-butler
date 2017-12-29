36fe8962f52b8834a9b6bc92f68cd7e51325f3bd
William Brooks <brooks.william@gmail.com>
2017-12-28 16:55:26 -0800

    moved cover to react component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
-import Sidebar from "./components/Sidebar";
 import Inbox from "./pages/Inbox";
+import Cover from "./pages/Cover";
+
+// import Sidebar from "./components/Sidebar";
 import StaticContainer from "./components/StaticContainer";
 
 const App = () => (
 	<Router>
 	<div>
-		<Sidebar />
 		<Switch>
+			<Route exact path="/" component={Cover} />
 			<Route exact path="/dashboard" component={Dashboard} />
 			<Route exact path="/settings" component={Settings} />
 			<Route exact path="/inbox" component={Inbox} />
