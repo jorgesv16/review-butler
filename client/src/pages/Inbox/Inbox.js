27f2958d24281b7c27308bb33c728f446efc57e3
William Brooks <brooks.william@gmail.com>
2017-12-29 09:05:41 -0800

    added sidebar to inbox and settings pages

--- client/src/pages/Inbox/Inbox.js
+++ client/src/pages/Inbox/Inbox.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
+import Sidebar from "../../components/Sidebar";
 import ReviewDetail from "../../components/ReviewDetail";
 import ReviewList from "../../components/ReviewList";
-
 
 class Inbox extends Component {
 	constructor(props) {
@@ -19,7 +19,7 @@ class Inbox extends Component {
 			.then(res =>
 				this.setState({
 					reviews: res.data,
-					selectedReviewID: res.data[0]._id  //set the initial review to the first review ID
+					selectedReviewID: res.data[0]._id //set the initial review to the first review ID
 				})
 			)
 			.catch(err => console.log(err));
@@ -28,14 +28,15 @@ class Inbox extends Component {
 	openReview(id) {
 		//set the review to read in database
 		const currentDate = Date.now();
-		API.updateReview(id, {review_read: currentDate})
-		.catch(err => console.log(err));
+		API.updateReview(id, { review_read: currentDate }).catch(err =>
+			console.log(err)
+		);
 
 		const reviews = this.state.reviews;
 		const index = reviews.findIndex(x => x._id === id);
 		reviews[index].review_read = currentDate;
 		this.setState({
-			selectedReviewID: id,
+			selectedReviewID: id
 		});
 	}
 
@@ -45,17 +46,18 @@ class Inbox extends Component {
 
 		//check if response_date exists, if so set to null
 		if (reviews[index].response_date) {
-			console.log('date exists')
-			API.updateReview(id, {response_date: null})
-			.catch(err => console.log(err));
+			console.log("date exists");
+			API.updateReview(id, { response_date: null }).catch(err =>
+				console.log(err)
+			);
 			reviews[index].response_date = null;
-		}
-		//the review has no date so set it
-		else {
-			console.log('no date set')
+		} else {
+			//the review has no date so set it
+			console.log("no date set");
 			const currentDate = Date.now();
-			API.updateReview(id, {response_date: currentDate})
-			.catch(err => console.log(err));
+			API.updateReview(id, { response_date: currentDate }).catch(err =>
+				console.log(err)
+			);
 			reviews[index].response_date = currentDate;
 		}
 		//update the state to refresh page
@@ -63,25 +65,31 @@ class Inbox extends Component {
 			selectedReviewID: id,
 			reviews: reviews
 		});
-
-
-
 	}
 
 	render() {
 		//get the current review
-		const currentReview = this.state.reviews.find(x => x._id === this.state.selectedReviewID);
+		const currentReview = this.state.reviews.find(
+			x => x._id === this.state.selectedReviewID
+		);
 		return (
 			<div>
-				<ReviewList
-					reviews={this.state.reviews}
-					onReviewSelected={(id) => { this.openReview(id); }}
-					selectedReviewID={this.state.selectedReviewID}
-				/>
-				<ReviewDetail 
-					review={currentReview} 
-					onRespondedClicked={(id) => {this.toggleResponded(id);}}
-				/>
+				<Sidebar />
+				<div>
+					<ReviewList
+						reviews={this.state.reviews}
+						onReviewSelected={id => {
+							this.openReview(id);
+						}}
+						selectedReviewID={this.state.selectedReviewID}
+					/>
+					<ReviewDetail
+						review={currentReview}
+						onRespondedClicked={id => {
+							this.toggleResponded(id);
+						}}
+					/>
+				</div>
 			</div>
 		);
 	}
