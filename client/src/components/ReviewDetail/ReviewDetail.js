38c0e4337c91da6bb197179f0b99053817cf66ce
William Brooks <brooks.william@gmail.com>
2018-01-02 15:31:18 -0800

    review responses showing in text field

--- client/src/components/ReviewDetail/ReviewDetail.js
+++ client/src/components/ReviewDetail/ReviewDetail.js
@@ -3,7 +3,7 @@ import "./ReviewDetail.css";
 import StarIcon from "../../components/StarIcon";
 import Sentence from "../../components/Sentence";
 import Tag from "../../components/Tag";
-import Form from "../../components/Form";
+import TextField from "material-ui/TextField";
 
 import Moment from "react-moment";
 
@@ -48,81 +48,60 @@ const ReviewDetail = ({ review, onRespondedClicked }) => {
 		}
 	});
 
+	const response = review.response_text;
+	
 	return (
 		<div className="review-container full-height review-detail">
 			<div className="flex-row">
 				<div className="review-user">
-					<h4>{review.user_name}</h4>
+				<h4>{review.user_name}</h4>
 				</div>
 			</div>
-
 			<div className="flex-row">
-					<img
-						className="review-logo"
-						src="img/yelp.png"
-						alt="Yelp Logo"
-					/>
+				<img
+					className="review-logo"
+					src="img/yelp.png"
+					alt="Yelp Logo"
+				/>
 				<div className="review-title">
-					<h2>{review.title}</h2>
+				<h2>{review.title ? review.title : "No Title"}</h2>
+					{review.title}
 				</div>
 				<div className="review-date">
-					<Moment date={review.response_date} format="MM/DD/YYYY" />
+					<Moment date={review.review_date} format="MM/DD/YYYY" />
 				</div>
 			</div>
-
-				<div className="review-rating">
-					<h4>{stars}</h4>
-				</div>
-
-				<div className="review-text">
-					{sentences}
-			</div>	
-							<div className="review-tags">
-					{tags}
-			</div>			
-
-
-				{review.response_text}{" "}
-				<button onClick={() => onRespondedClicked(review._id)}>
-					set responded
-				</button>
-
+			<div className="review-rating">
+				<h4>{stars}</h4>
+			</div>
+			<div className="review-text">{sentences}</div>
+			<div className="flex-row">
+				<div className="review-tags"><span className="tags-title">Tags: </span> {tags}</div>
+			</div>
+			<br/>
+			<h2>Response</h2>
+			<TextField
+				multiLine={true}
+				fullWidth={true}
+				value={response}
+			/>
+			<br />
+			<button onClick={() => onRespondedClicked(review._id)}>
+				set responded
+			</button>
 			<li>
 				<strong>sentiment_score: </strong>
 				{review.sentiment_score}
-			</li>
-			<li>
-				<strong>word_count: </strong>
-				{review.word_count}
-			</li>
-			<li>
-				<strong>review_date: </strong>
-				<Moment date={review.review_date} format="MM/DD/YYYY" />
 			</li>
 			<li>
 				<strong>response_date: </strong>
 				{review.response_date}
 			</li>
 			<li>
-				<strong>photo: </strong>
-				{review.photo}
-			</li>
-			<li>
-				<strong>review_read: </strong>
-				{review.review_read}
-			</li>
-			<li>
 				<strong>business_id: </strong>
 				{review.business_id}
 			</li>
-			<li>
-				<strong>timestamp: </strong>
-				{review.timestamp}
-			</li>
-			<li>
-				<strong>external_id: </strong>
-				{review.external_id}
-			</li>
+
 		</div>
 	);
 };
