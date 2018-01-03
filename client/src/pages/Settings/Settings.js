45f1dfe8707e1218d8cc4461e869fe4b7f8c7abf
Jorge <jorge@github.com>
2018-01-02 17:16:21 -0800

    settings page

--- client/src/pages/Settings/Settings.js
+++ client/src/pages/Settings/Settings.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
+import {Button, Form, FormGroup, Label, Input, Col, Row, Container} from 'reactstrap';
 import Sidebar from "../../components/Sidebar";
+import "./Settings.css";
 
-class Inbox extends Component {
+class Settings extends Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -11,12 +13,53 @@ class Inbox extends Component {
     return (
       <div>
         <Sidebar />
-        <div className="float-md-left">
-          <div className="dashboard-container full-height">Insert Settings</div>
+        <div className="settings-container full-height settings-detail">
+          <div className="flex-row">
+            <section className="signup-sec parallax-section">
+              <Container>
+                <Row>
+                  <Col lg="auto">
+                    <Form
+                      onSubmit={this.processForm}
+                      onChange={this.changeUser}
+                      errors={this.state.errors}
+                      // successMessage={this.state.successMessage}
+                      user={this.state.user}
+                      action="/login">
+                      <FormGroup>
+                        <Label for="userEmail">Display Name</Label>
+                        <Input className="form-control input-lg" type="email" name="name" id="login-email" placeholder=""/>
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="userPassword">Email</Label>
+                        <Input className="form-control input-lg" type="email" name="email" id="login-email" placeholder=""/>
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="userPassword">Phone</Label>
+                        <Input className="form-control input-lg" type="phone" name="password" id="login-password" placeholder=""/>
+                      </FormGroup>
+                      <FormGroup>
+                        <Label for="userPassword">Occupation</Label>
+                        <Input className="form-control input-lg" type="text" name="password" id="login-password" placeholder=""/>
+                      </FormGroup>
+                      <FormGroup check>
+                        <Label check>
+                          <Input type="checkbox" />{' '}
+                          Display occupation on responses
+                        </Label>
+                      </FormGroup>
+                      <Button className="btn btn-lg btn-primary btn-block signup-btn" id="submit">Save</Button>
+                    </Form>
+                  </Col>
+                </Row>
+              </Container>
+            </section>
+          </div>
         </div>
       </div>
     );
   }
-}
+};
 
-export default Inbox;
+export default Settings;
+ 
